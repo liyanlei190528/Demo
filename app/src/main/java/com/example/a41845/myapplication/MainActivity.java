8210aabc5ce3804437a2ceafb832be2388411098
liyanlei190528 <418456070@qq.com>
2019-05-29 15:42:15 +0800

    开始第一天

--- app/src/main/java/com/example/a41845/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a41845/myapplication/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.a41845.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Hello World!
+     */
+    private TextView mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBtn = (TextView) findViewById(R.id.btn);
     }
 }
